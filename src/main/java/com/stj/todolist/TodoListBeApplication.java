9308eea1f06f54a024c4a2647620ff3adc3b966d
stj0215 <stj960215@gmail.com>
2021-05-08 16:36:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/stj/todolist/TodoListBeApplication.java
@@ -0,0 +1,13 @@
+package com.stj.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stj/todolist/TodoListBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stj.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
